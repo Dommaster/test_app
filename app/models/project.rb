f73a187f028416450d9e29bcd870ee39fb5a549d
Dominik Buetow <dominik.buetow@gmail.com>
2014-01-23 06:59:44 +1100

    just a test for the test app

---
+++ app/models/project.rb
@@ -0,0 +1,3 @@
+class Project < ActiveRecord::Base
+  attr_accessible :hours, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c9fd2d4f5b99a74ffd86df7eafb045ea3805ecfa86659981ff0c17132431420aa72c2d46a61dc0a0790e6744982653c3c98e5a8b05eecd814b2dc16683c36a33'
